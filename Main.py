5d80d6907938731764086e8e2413b5284a318957
urk21co3026 <110015042+urk21co3026@users.noreply.github.com>
2022-08-01 23:29:40 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ class Evaluate:
     # Write your code here
 
 
-  def __init__(self, size):
+  def _init_(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
       size_of_stack: An integer to set the size of stack.
@@ -19,17 +19,28 @@ class Evaluate:
 
 
   def isEmpty(self):
+    """
+    Check whether the stack is empty.
+    Returns:
+      True if it is empty, else returns False.
+    """
+      # Write your code here
     if len(self.stack) == 0:
-      return True
+        return True
     else:
-      return False
+        return False
 
 
   def pop(self):
+    """
+    Do pop operation if the stack is not empty.
+    Returns:
+      The data which is popped out if the stack is not empty.
+    """
+    # Write your code here
     if len(self.stack) > 0:
-      x = self.stack.pop()
-      return x
-   
+        x = self.stack.pop()
+        return x
 
 
   def push(self, operand):
@@ -37,42 +48,63 @@ class Evaluate:
     Push the operand to stack if the stack is not full.
     Arguments:
       operand: The operand to be pushed.
-      self.stack.append(operand)
     """
+    # Write your code here
     self.stack.append(operand)
 
 
   def validate_postfix_expression(self, expression):
+    """
+    Check whether the expression is a valid postfix expression.
+    Arguments:
+      expression: A String which represents the expression to be validated.
+    Returns:
+      True if the expression is valid, else returns False.
+    """
+    # Write your code here
     value = True
-    valid = ['+','-','*','/']
-    for char in expression:
-      if char.isdigit or char in valid:
-        continue
-      else:
-        value = False
-        return value
     
+    valid = ['+','-','*','/']
+    
+    for char in expression:
+        if char.isdigit or char in valid:
+            continue
+        else:
+            value = False
+    
+    return value
 
 
   def evaluate_postfix_expression(self, expression):
+    """
+    Evaluate the postfix expression
+    Arguments:
+      expression: A String which represents the the expression to be evaluated
+    Returns:
+      The result of evaluated postfix expression.
+    """
+    
+    # Write your code here
+    
     for char in expression:
-      if char.isdigit():
-        self.push(char)
-      else:
-        b = int(self.pop())
-        a = int(self.pop())
-      if char == "+":
-        result = a + b
-      elif char == "-":
-        result = a - b
-      elif char == '*':
-        result = a * b
-      elif char == '/':
-        result = a / b
-      self.push(result)
+        if char.isdigit():
+            self.push(char)
+        else:
+            b = int(self.pop())
+            a = int(self.pop())
+            
+            if char == "+":
+                result = a + b
+            elif char == "-":
+                result = a - b
+            elif char == '*':
+                result = a * b
+            elif char == '/':
+                result = a / b
+            
+            self.push(result)
     
     return int(self.stack[0])
-  
 
 
 # Do not change the following code
